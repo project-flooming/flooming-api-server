d1d97819582eb9eeb031e34f86e065379142ca90
Seong Heung Bae <mopil1102@gmail.com>
2022-07-14 13:36:36 +0900

    예외처리 디테일 추가

--- routers/photos.py
+++ routers/photos.py
@@ -3,7 +3,7 @@ import uuid
 
 from fastapi import APIRouter, UploadFile, Depends
 from sqlalchemy.orm import Session
-from starlette.responses import FileResponse
+from starlette.responses import FileResponse, JSONResponse
 
 from ai.inference import classify
 from database.config import get_db
@@ -17,48 +17,52 @@ router = APIRouter()
 # 사진 업로드 후 꽃 분류
 @router.post("/photo")
 async def upload_photo(file: UploadFile, db: Session = Depends(get_db)):
-    # 서버 로컬 스토리지에 이미지 저장
-    UPLOAD_DIR = "./photo"
+    try:
+        # 서버 로컬 스토리지에 이미지 저장
+        UPLOAD_DIR = "./photo"
 
-    content = await file.read()
-    logger.info(f"original filename = {file.filename}")
-    filename = f"{str(uuid.uuid4())}.jpg"
-    with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
-        fp.write(content)
-    src = f"{UPLOAD_DIR}/{filename}"
+        content = await file.read()
+        logger.info(f"original filename = {file.filename}")
+        filename = f"{str(uuid.uuid4())}.jpg"
+        with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
+            fp.write(content)
+        src = f"{UPLOAD_DIR}/{filename}"
 
-    # 디비에 저장
-    db_photo = Photo(filename=filename, src=src)
-    db.add(db_photo)
-    db.commit()
-    db.refresh(db_photo)
+        # 디비에 저장
+        db_photo = Photo(filename=filename, src=src)
+        db.add(db_photo)
+        db.commit()
+        db.refresh(db_photo)
 
-    # 꽃 분류
-    result_response = []
-    classify_result = classify(src)
+        # 꽃 분류
+        result_response = []
+        classify_result = classify(src)
 
-    prob = [result["probability"] for result in classify_result]
-    gap1 = abs(prob[0] - prob[1])
-    gap2 = abs(prob[1] - prob[2])
-    gap3 = abs(prob[0] - prob[2])
-    if gap1 <= 1 and gap2 <= 1 and gap3 <= 1:
-        return {"result": "anomaly"}
+        prob = [result["probability"] for result in classify_result]
+        gap1 = abs(prob[0] - prob[1])
+        gap2 = abs(prob[1] - prob[2])
+        gap3 = abs(prob[0] - prob[2])
+        if gap1 <= 1 and gap2 <= 1 and gap3 <= 1:
+            return {"result": "anomaly"}
 
-    logger.info("분류 결과 = {}", classify_result)
-    for result in classify_result:
-        flower: Flower = db.query(Flower).filter_by(kor_name=result["type"]).first()
-        if flower is None:
-            continue
-        data = {
-            "probability": result["probability"],
-            "kor_name": flower.kor_name,
-            "eng_name": flower.eng_name,
-            "flower_language": flower.flower_language,
-            "img_src": flower.img_src
-        }
-        result_response.append(data)
+        logger.info("분류 결과 = {}", classify_result)
+        for result in classify_result:
+            flower: Flower = db.query(Flower).filter_by(kor_name=result["type"]).first()
+            if flower is None:
+                continue
+            data = {
+                "probability": result["probability"],
+                "kor_name": flower.kor_name,
+                "eng_name": flower.eng_name,
+                "flower_language": flower.flower_language,
+                "img_src": flower.img_src
+            }
+            result_response.append(data)
 
-    return {"result": result_response}
+        return {"result": result_response}
+    except Exception as e:
+        logger.warning(f"Upload and Classify Error : {str(e)}")
+        return JSONResponse(content={"error": "꽃 사진을 분류하는데 오류가 발생했습니다. 다시 시도해 주세요."}, status_code=400)
 
 
 # 사진 타입 변경 (사용자가 사진 최종 선택)
@@ -72,24 +76,36 @@ async def update_photo_type(photo_id: int, flower_type: str, db: Session = Depen
 # 사진 가져오기
 @router.get("/photo/{photo_id}")
 async def get_photo_by_id(photo_id, db: Session = Depends(get_db)):
-    return db.query(Photo).filter_by(photo_id=photo_id).first()
+    try:
+        return db.query(Photo).filter_by(photo_id=photo_id).first()
+    except Exception as e:
+        logger.warning(f"Photo fetch error : {str(e)}")
+        return JSONResponse(content={"error": "꽃 사진을 가져오는데 오류가 발생했습니다."}, status_code=400)
 
 
 # 사진 -> 그림 변환
 @router.get("/drawing/{photo_id}")
 async def create_picture(photo_id: int, db: Session = Depends(get_db)):
-    # 그림 변환
-    # 그림 디비 저장
-    db_picture: Picture = Picture(src="unknown")
-    db.add(db_picture)
-    db.commit()
-    db.refresh(db_picture)
+    try:
+        # 그림 변환
+        # 그림 디비 저장
+        db_picture: Picture = Picture(src="unknown")
+        db.add(db_picture)
+        db.commit()
+        db.refresh(db_picture)
 
-    return db_picture
+        return db_picture
+    except Exception as e:
+        logger.warning(f"Drawing error : {str(e)}")
+        return JSONResponse(content={"error": "꽃 사진을 그림으로 변환하는데 오류가 발생했습니다. 다시 시도해 주세요."}, status_code=400)
 
 
 # 사진 다운로드
 @router.get("/download/photo/{photo_id}")
 async def download_photo(photo_id: int, db: Session = Depends(get_db)):
-    find_photo: Photo = db.query(Photo).filter_by(photo_id=photo_id).first()
-    return FileResponse(find_photo.src)
+    try:
+        find_photo: Photo = db.query(Photo).filter_by(photo_id=photo_id).first()
+        return FileResponse(find_photo.src)
+    except Exception as e:
+        logger.warning(f"Photo download error : {str(e)}")
+        return JSONResponse(content={"error": "꽃 사진을 저장하는데 오류가 발생했습니다. 다시 시도해 주세요."}, status_code=400)
--- routers/pictures.py
+++ routers/pictures.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy import desc
 from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
 
 from database.config import get_db
 from database.models import Picture, Gallery, Photo
 from database.schemas import GalleryRequest
+from loguru import logger
 
 router = APIRouter()
 
@@ -12,24 +14,32 @@ router = APIRouter()
 # 갤러리 - 사진/그림 업로드
 @router.post("/picture")
 async def create_gallery(form: GalleryRequest, db: Session = Depends(get_db)):
-    find_photo: Photo = db.query(Photo).filter_by(photo_id=form.photo_id).first()
-    find_picture: Picture = db.query(Picture).filter_by(picture_id=form.picture_id).first()
+    try:
+        find_photo: Photo = db.query(Photo).filter_by(photo_id=form.photo_id).first()
+        find_picture: Picture = db.query(Picture).filter_by(picture_id=form.picture_id).first()
 
-    db_gallery = Gallery(photo_src=find_photo.src, picture_src=find_picture.src, comment=form.comment)
+        db_gallery = Gallery(photo_src=find_photo.src, picture_src=find_picture.src, comment=form.comment)
 
-    db.add(db_gallery)
-    db.commit()
-    db.refresh(db_gallery)
+        db.add(db_gallery)
+        db.commit()
+        db.refresh(db_gallery)
 
-    return db_gallery
+        return db_gallery
+    except Exception as e:
+        logger.warning(f"Gallery upload error : {str(e)}")
+        return JSONResponse(content={"error": "갤러리에 그림을 전시하는데 오류가 발생했습니다. 다시 시도해 주세요."}, status_code=400)
 
 
 # 갤러리 - 사진/그림 리스트 반환
 @router.get("/gallery")
 async def get_all_gallery(page: int, db: Session = Depends(get_db)):
-    unit_per_page = 5
-    offset = page * unit_per_page
-    return db.query(Gallery).order_by(desc(Gallery.created_time)).offset(offset).limit(unit_per_page).all()
+    try:
+        unit_per_page = 5
+        offset = page * unit_per_page
+        return db.query(Gallery).order_by(desc(Gallery.created_time)).offset(offset).limit(unit_per_page).all()
+    except Exception as e:
+        logger.warning(f"Gallery list fetching error : {str(e)}")
+        return JSONResponse(content={"error": "갤러리 그림들을 가져오는데 오류가 발생했습니다. 다시 시도해 주세요."}, status_code=400)
 
 
 # 그림 다운로드
