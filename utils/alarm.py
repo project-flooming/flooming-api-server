4da9101cdee8c12622e49f6b311809c6100ed934
Seong Heung Bae <mopil1102@gmail.com>
2022-08-27 18:46:16 +0900

    feat: 슬랙 알람 메시지에 링크 추가

--- utils/alarm.py
+++ utils/alarm.py
@@ -9,7 +9,8 @@ def send_slack_alarm(new_report: Report):
     hook = secrets['slack']
     time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     title = f"[{time}] \n새로운 신고 접수!"
-    content = f"\n갤러리 ID : {new_report.gallery_id} \n사유 : {new_report.detail}"
+    content = f"\n갤러리 ID : {new_report.gallery_id} \n사유 : {new_report.detail}"\
+              f"\nhttp://flooming.link/admin"
 
     requests.post(
         hook,
